960f044dc865e45f55463d25c032120140e5673d
jarekjal <jarekjal@wp.pl>
2023-09-30 19:17:52 +0200

    ex5 - solution

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,21 +1,53 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumberFlux() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException().subscribe(
+                n -> System.out.println("num: " + n),
+                err -> System.out.println(err.getMessage()),
+                () -> System.out.println("flux completed")
+        );
+
+        ReactiveSources.userMono().subscribe(
+                user -> System.out.println(user),
+                error -> System.out.println(error.getMessage()),
+                () -> System.out.println("mono completed")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                .subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
+
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("subscribe happened");
+        request(1);
+    }
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.print(value.toString() + "received, ");
+        System.out.println(Thread.currentThread());
+        request(1);
+    }
+
 
 }
